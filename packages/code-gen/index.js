69a6c6c023cac514a040b06c3be398e2f3d38dc4
Dirk <dirk@lightbase.nl>
2020-02-20 13:34:34 +0100

    [code-gen]: Port validators to the new template system
    I may move all templates to their own files to keep the plugin cleaner, but then the stdlib / code-gen should have
    a helpper that loads the templates in

--- packages/code-gen/index.js
+++ packages/code-gen/index.js
@@ -1,3 +1,5 @@
+const { getPlugin: getValidatorPlugin } = require("./src/validators");
+
 /*
  THE BATTLEPLAN:
 
@@ -25,8 +27,148 @@
  Interface from core to plugins for preInit stage:
  ```
  {
-   hasPlugin(name: string): boolean;
-   options: All general options except the plugin array
+ hasPlugin(name: string): boolean;
+ options: All general options except the plugin array
  }
  ```
  */
+
+const validators = [
+  {
+    type: "NAMED_VALIDATOR",
+    validatorName: "validateMyBool",
+    preValidateHook: "preValidateMyBool",
+    postValidateHook: "postValidateMyBool",
+    validator: {
+      type: "boolean",
+      functionName: "booleanValidator1",
+      optional: true,
+      convert: true,
+      oneOf: false,
+    },
+  },
+  {
+    type: "NAMED_VALIDATOR",
+    validatorName: "validateMyNumber",
+    preValidateHook: "preValidateMyNumber",
+    postValidateHook: "postValidateMyNumber",
+    validator: {
+      type: "number",
+      functionName: "numberValidator1",
+      optional: true,
+      oneOf: [1, 2, 3, 4],
+      convert: true,
+      min: 5,
+      max: 10,
+      integer: true,
+    },
+  },
+  {
+    type: "NAMED_VALIDATOR",
+    validatorName: "validateMyString",
+    preValidateHook: "preValidateMyString",
+    postValidateHook: "postValidateMyString",
+    validator: {
+      type: "string",
+      functionName: "stringValidator1",
+      optional: true,
+      oneOf: ["foo", "bar"],
+      convert: true,
+      min: 5,
+      max: 10,
+      trim: true,
+      lowerCase: false,
+      upperCase: true,
+      pattern: undefined,
+    },
+  },
+  {
+    type: "NAMED_VALIDATOR",
+    validatorName: "validateMyObject",
+    preValidateHook: "preValidateMyObject",
+    postValidateHook: "postValidateMyObject",
+    validator: {
+      type: "object",
+      functionName: "objectValidator1",
+      optional: false,
+      strict: true,
+      keys: {
+        foo: {
+          type: "boolean",
+          functionName: "booleanValidator2",
+        },
+        bar: {
+          type: "object",
+          functionName: "objectValidator2",
+          optional: true,
+          keys: {
+            foo: {
+              type: "boolean",
+              functionName: "booleanValidator3",
+            },
+          },
+        },
+        baz: {
+          type: "number",
+          functionName: "numberValidator3",
+        },
+      },
+    },
+  },
+  {
+    type: "NAMED_VALIDATOR",
+    validatorName: "validateMyArray",
+    preValidateHook: "preValidateMyArray",
+    postValidateHook: "postValidateMyArray",
+    validator: {
+      type: "array",
+      functionName: "arrayValidator1",
+      optional: true,
+      convert: false,
+      min: 2,
+      values: {
+        type: "boolean",
+        functionName: "booleanValidator6",
+      },
+    },
+  },
+  {
+    type: "NAMED_VALIDATOR",
+    validatorName: "validateMyAnyOf",
+    preValidateHook: "preValidateMyAnyOf",
+    postValidateHook: "postValidateMyAnyOf",
+    validator: {
+      type: "anyOf",
+      functionName: "anyOfValidator1",
+      optional: true,
+      values: [
+        {
+          type: "boolean",
+          functionName: "booleanValidator7",
+          optional: true,
+        },
+        {
+          type: "boolean",
+          functionName: "booleanValidator8",
+          convert: true,
+        },
+      ],
+    },
+  },
+  {
+    type: "NAMED_VALIDATOR",
+    validatorName: "validateMyReference",
+    preValidateHook: "preValidateMyReference",
+    postValidateHook: "postValidateMyReference",
+    validator: {
+      type: "reference",
+      functionName: "referenceValidator1",
+      optional: true,
+      reference: "arrayValidator1",
+    },
+  },
+];
+
+const validatorPlugin = getValidatorPlugin();
+validatorPlugin.init();
+validatorPlugin.run({ outputDir: process.cwd(), data: { validators } });
