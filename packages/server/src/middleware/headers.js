dd21618099d7cca44a86f27e33f57ecb48c197e7
Dirk de Visser <dirkdev98@gmail.com>
2020-03-09 20:50:30 +0100

    server: Drop helmet. Most of default headers are only useful when serving files

--- packages/server/src/middleware/headers.js
+++ packages/server/src/middleware/headers.js
@@ -1,4 +1,3 @@
-import koaHelmet from "koa-helmet";
 import cors from "koa2-cors";
 
 const noop = () => {};
@@ -6,16 +5,21 @@ const noop = () => {};
 /**
  * TODO: Replace with custom code calling Helmet, and custom cors implementation
  * @param {Object} [opts=]
- * @param {Object=} opts.helmet Helmet configuration see koa-helmet
  * @param {Object=} opts.cors Cors configuration see koa2-cors
  */
 export const defaultHeaders = (opts = {}) => {
-  const helmetExec = koaHelmet(opts.helmet);
+  // Excerpt from default helmet headers
+  // When serving static files, some extra headers should be added
+  // See: https://helmetjs.github.io/docs/
+  const standardHeaders = {
+    "X-DNS-Prefetch-Control": "off",
+    "Strict-Transport-Security": "max-age=5184000; includeSubDomains", // 60 day default
+  };
   const corsExec = cors(opts.cors);
 
   return async (ctx, next) => {
     // At the moment, both do not rely on next, so just use a noop
-    await helmetExec(ctx, noop);
+    ctx.set(standardHeaders);
     await corsExec(ctx, noop);
 
     return next();
