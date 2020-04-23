2a0300efa44ff17baf7891ee36a0e416e2f78855
Dirk de Visser <dirkdev98@gmail.com>
2020-04-23 21:04:45 +0200

    server: remove global state of body parsers

--- packages/server/index.js
+++ packages/server/index.js
@@ -4,8 +4,6 @@ export { compose };
 export { getApp } from "./src/app.js";
 export {
   createBodyParsers,
-  getBodyParser,
-  getMultipartBodyParser,
   AppError,
   isServerLog,
 } from "./src/middleware/index.js";
--- packages/server/src/middleware/body.js
+++ packages/server/src/middleware/body.js
@@ -1,9 +1,6 @@
 import { merge } from "@lbu/stdlib";
 import koaBody from "koa-body";
 
-let memoizeBodyParser;
-let memoizeMultipartBodyParser;
-
 /**
  * Creates a body parser and a body parser with multipart enabled
  * Note that koa-body parses url-encoded, form data, json and text by default
@@ -15,37 +12,8 @@ export function createBodyParsers(opts = {}) {
   opts.multipart = false;
   multiPartOpts.multipart = true;
 
-  memoizeBodyParser = koaBody(opts);
-  memoizeMultipartBodyParser = koaBody(multiPartOpts);
-}
-
-/**
- * Middleware to parse querystring and request body
- * Only use this before a route that needs access to ctx.request.body or ctx.request.query
- * @returns {*} Koa middleware
- */
-export function getBodyParser() {
-  if (memoizeBodyParser === undefined) {
-    throw new Error(
-      "Body parser used before initialization. Call createBodyParsers first",
-    );
-  }
-
-  return memoizeBodyParser;
-}
-
-/**
- * Middleware to parse querystring and request body with file support
- * Only use this before a route that needs access to ctx.request.body, ctx.request.files
- * or ctx.request.query
- * @returns {*} Koa middleware
- */
-export function getMultipartBodyParser() {
-  if (memoizeMultipartBodyParser === undefined) {
-    throw new Error(
-      "Multipart body parser used before initialization. Call createBodyParsers first",
-    );
-  }
-
-  return memoizeMultipartBodyParser;
+  return {
+    bodyParser: koaBody(opts),
+    multipartBodyParser: koaBody(multiPartOpts),
+  };
 }
