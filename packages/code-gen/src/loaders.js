d970ccfaff261d04d02fcf29288e4faff1860501
Dirk de Visser <dirkdev98@gmail.com>
2020-05-24 16:14:01 +0200

    code-gen: add OpenAPI loader
    Tried it on the Pet store swagger example, and on a internal document.
    Seems to work ok. The biggest issue currently is the use of long descriptive names in some OpenAPI specs that I looked at. This meant that prettier will put for example the variable name on a new line in JSDoc which results in an invalid line and thus a 'useless' stub.

--- packages/code-gen/src/loaders.js
+++ packages/code-gen/src/loaders.js
@@ -1,7 +1,23 @@
+import { isPlainObject } from "@lbu/stdlib";
 import axios from "axios";
+import { convertOpenAPISpec } from "./open-api-importer.js";
 
 export async function loadFromRemote(url) {
   const response = await axios.get(url + "/_lbu/structure.json");
 
   return response.data;
+}
+
+/**
+ * Try to convert a OpenAPI spec object to LBU structure
+ * @param {string} defaultGroup Default to group to use for non tagged items in the spec
+ * @param {object} data
+ * @return {object}
+ */
+export function loadFromOpenAPISpec(defaultGroup, data) {
+  if (!isPlainObject(data)) {
+    throw new TypeError("Expecting a plain js object");
+  }
+
+  return convertOpenAPISpec(defaultGroup, data);
 }
