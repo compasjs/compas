9fef98d000d5fad6d84945b40a4eee74ee697d03
Dirk de Visser <dirkdev98@gmail.com>
2020-06-07 19:31:48 +0200

    lint-config: add JSDoc linter
    References #40

--- packages/lint-config/index.js
+++ packages/lint-config/index.js
@@ -1,5 +1,5 @@
 /**
- * @type {Object} Eslint settings
+ * @type {object} Eslint settings
  */
 module.exports = {
   extends: [
@@ -7,8 +7,15 @@ module.exports = {
     "plugin:prettier/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
+    "plugin:jsdoc/recommended",
   ],
   parser: "babel-eslint",
+  settings: {
+    jsdoc: {
+      mode: "typescript",
+      ignorePrivate: true,
+    },
+  },
   rules: {
     "no-process-exit": "off",
   },
