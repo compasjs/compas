022caa1e711c2d03adb481470300bc80834a98a6
Dirk de Visser <dirkdev98@gmail.com>
2020-06-11 11:46:14 +0200

    lint-config: drop jsdoc plugin
    It is too intrusive, and shouldn't be forced upon users of lbu.
    It helped a great deal in getting all JSDoc in this repo aligned, which at least solved part of our problems

--- packages/lint-config/index.js
+++ packages/lint-config/index.js
@@ -7,19 +7,10 @@ module.exports = {
     "plugin:prettier/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
-    "plugin:jsdoc/recommended",
   ],
   parser: "babel-eslint",
-  settings: {
-    jsdoc: {
-      mode: "typescript",
-      ignorePrivate: true,
-    },
-  },
   rules: {
     "no-process-exit": "off",
-    "jsdoc/require-returns-description": "off",
-    "jsdoc/require-property": "off",
   },
   env: {
     node: true,
