7c23c8bd7ad15f6b0b5d650d98c5855495de37ea
Dirk de Visser <dirkdev98@gmail.com>
2020-06-29 13:58:40 +0200

    code-gen: always quote object keys
    Prettier will strip unnecessary ones from typescript files. For JSDoc it may visually clutter the definition a bit. Not sure yet how I feel about this one.

--- packages/store/src/generated/types.js
+++ packages/store/src/generated/types.js
@@ -4,43 +4,43 @@
 /**
  * @name StoreFileStore
  *
- * @typedef { { id:string, bucketName:string, contentLength:number, contentType:string, filename:string, createdAt:Date, updatedAt:Date, }}
+ * @typedef { { "id":string, "bucketName":string, "contentLength":number, "contentType":string, "filename":string, "createdAt":Date, "updatedAt":Date, }}
  */
 
 /**
  * @name StoreFileStore_Input
  *
- * @typedef { { id:string, bucketName:string, contentLength:number, contentType:string, filename:string, createdAt?:string, updatedAt?:string, }}
+ * @typedef { { "id":string, "bucketName":string, "contentLength":number, "contentType":string, "filename":string, "createdAt"?:string, "updatedAt"?:string, }}
  */
 
 /**
  * @name StoreSessionStore
  *
- * @typedef { { id:string, expires:Date, data:*, createdAt:Date, updatedAt:Date, }}
+ * @typedef { { "id":string, "expires":Date, "data":*, "createdAt":Date, "updatedAt":Date, }}
  */
 
 /**
  * @name StoreSessionStore_Input
  *
- * @typedef { { id:string, expires:string, data?:* , createdAt?:string, updatedAt?:string, }}
+ * @typedef { { "id":string, "expires":string, "data"?:* , "createdAt"?:string, "updatedAt"?:string, }}
  */
 
 /**
  * @name StoreJobQueue
  *
- * @typedef { { id:number, isComplete:boolean, priority:number, scheduledAt:Date, name:string, data:*, createdAt:Date, updatedAt:Date, }}
+ * @typedef { { "id":number, "isComplete":boolean, "priority":number, "scheduledAt":Date, "name":string, "data":*, "createdAt":Date, "updatedAt":Date, }}
  */
 
 /**
  * @name StoreJobQueue_Input
  *
- * @typedef { { id:number, isComplete?:boolean , priority?:number , scheduledAt?:string, name:string, data?:* , createdAt?:string, updatedAt?:string, }}
+ * @typedef { { "id":number, "isComplete"?:boolean , "priority"?:number , "scheduledAt"?:string, "name":string, "data"?:* , "createdAt"?:string, "updatedAt"?:string, }}
  */
 
 /**
  * @name StoreFileStoreWhere
  *
- * @typedef { { id?:string, idIn?:(string)[] , bucketName?:string , bucketNameLike?:string , }}
+ * @typedef { { "id"?:string, "idIn"?:(string)[] , "bucketName"?:string , "bucketNameLike"?:string , }}
  */
 
 /**
@@ -52,43 +52,43 @@
 /**
  * @name StoreFileStoreInsertPartial
  *
- * @typedef { { bucketName:string, contentLength:number, contentType:string, filename:string, createdAt:Date, updatedAt:Date, }}
+ * @typedef { { "bucketName":string, "contentLength":number, "contentType":string, "filename":string, "createdAt":Date, "updatedAt":Date, }}
  */
 
 /**
  * @name StoreFileStoreInsertPartial_Input
  *
- * @typedef { { bucketName:string, contentLength:number, contentType:string, filename:string, createdAt?:string, updatedAt?:string, }}
+ * @typedef { { "bucketName":string, "contentLength":number, "contentType":string, "filename":string, "createdAt"?:string, "updatedAt"?:string, }}
  */
 
 /**
  * @name StoreSessionStoreWhere
  *
- * @typedef { { id?:string, idIn?:(string)[] , expires?:Date, expiresGreaterThan?:Date, expiresLowerThan?:Date, }}
+ * @typedef { { "id"?:string, "idIn"?:(string)[] , "expires"?:Date, "expiresGreaterThan"?:Date, "expiresLowerThan"?:Date, }}
  */
 
 /**
  * @name StoreSessionStoreWhere_Input
  *
- * @typedef { { id?:string, idIn?:(string)[] , expires?:string, expiresGreaterThan?:string, expiresLowerThan?:string, }}
+ * @typedef { { "id"?:string, "idIn"?:(string)[] , "expires"?:string, "expiresGreaterThan"?:string, "expiresLowerThan"?:string, }}
  */
 
 /**
  * @name StoreSessionStoreInsertPartial
  *
- * @typedef { { expires:Date, data:*, createdAt:Date, updatedAt:Date, }}
+ * @typedef { { "expires":Date, "data":*, "createdAt":Date, "updatedAt":Date, }}
  */
 
 /**
  * @name StoreSessionStoreInsertPartial_Input
  *
- * @typedef { { expires:string, data?:* , createdAt?:string, updatedAt?:string, }}
+ * @typedef { { "expires":string, "data"?:* , "createdAt"?:string, "updatedAt"?:string, }}
  */
 
 /**
  * @name StoreJobQueueWhere
  *
- * @typedef { { id?:number , idGreaterThan?:number , idLowerThan?:number , name?:string , nameLike?:string , }}
+ * @typedef { { "id"?:number , "idGreaterThan"?:number , "idLowerThan"?:number , "name"?:string , "nameLike"?:string , }}
  */
 
 /**
@@ -100,11 +100,11 @@
 /**
  * @name StoreJobQueueInsertPartial
  *
- * @typedef { { isComplete:boolean, priority:number, scheduledAt:Date, name:string, data:*, createdAt:Date, updatedAt:Date, }}
+ * @typedef { { "isComplete":boolean, "priority":number, "scheduledAt":Date, "name":string, "data":*, "createdAt":Date, "updatedAt":Date, }}
  */
 
 /**
  * @name StoreJobQueueInsertPartial_Input
  *
- * @typedef { { isComplete?:boolean , priority?:number , scheduledAt?:string, name:string, data?:* , createdAt?:string, updatedAt?:string, }}
+ * @typedef { { "isComplete"?:boolean , "priority"?:number , "scheduledAt"?:string, "name":string, "data"?:* , "createdAt"?:string, "updatedAt"?:string, }}
  */
