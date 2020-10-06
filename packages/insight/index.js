924e5ed33556c607148d510f6afc031f926e3e4b
Dirk de Visser <dirkdev98@gmail.com>
2020-10-06 14:02:11 +0200

    insight: add basic event system
    Consists of manually calling eventStart, eventStop, ..., and by design controlled by the event implementation and not by the caller.

--- packages/insight/index.js
+++ packages/insight/index.js
@@ -3,6 +3,14 @@ import { newLogger } from "./src/logger/logger.js";
 export { bytesToHumanReadable, printProcessMemoryUsage } from "./src/memory.js";
 export { newLogger, bindLoggerContext } from "./src/logger/logger.js";
 
+export {
+  newEvent,
+  eventStart,
+  eventRename,
+  eventStop,
+  newTestEvent,
+  newEventFromEvent,
+} from "./src/events.js";
 export { postgresTableSizes } from "./src/postgres.js";
 
 /**
