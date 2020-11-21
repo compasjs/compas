75bea4aed730ece0fa27b93ac2d530587fefe985
Dirk de Visser <dirkdev98@gmail.com>
2020-11-21 14:14:25 +0100

    code-gen: deduplicate values passed to anyOf
    For a cleaner type and validator output

--- packages/code-gen/src/builders/AnyOfType.js
+++ packages/code-gen/src/builders/AnyOfType.js
@@ -1,9 +1,33 @@
+import { inspect } from "util";
 import { isNil } from "@lbu/stdlib";
 import { TypeBuilder } from "./TypeBuilder.js";
 import { buildOrInfer } from "./utils.js";
 
 export class AnyOfType extends TypeBuilder {
   static baseData = {};
+
+  build() {
+    const result = super.build();
+
+    const set = new Set();
+    result.values = [];
+
+    for (const v of this.internalValues) {
+      const buildValue = buildOrInfer(v);
+      const stringValueOfBuild = inspect(buildValue, {
+        colors: false,
+        pretty: false,
+        depth: 18, // About 5 levels of object types
+      });
+
+      if (!set.has(stringValueOfBuild)) {
+        set.add(stringValueOfBuild);
+        result.values.push(buildValue);
+      }
+    }
+
+    return result;
+  }
 
   constructor(group, name) {
     super("anyOf", group, name);
@@ -12,17 +36,6 @@ export class AnyOfType extends TypeBuilder {
       ...this.data,
       ...AnyOfType.getBaseData(),
     };
-  }
-
-  build() {
-    const result = super.build();
-
-    result.values = [];
-    for (const v of this.internalValues) {
-      result.values.push(buildOrInfer(v));
-    }
-
-    return result;
   }
 
   /**
