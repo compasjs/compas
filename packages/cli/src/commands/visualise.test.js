3358a9f25938f932ee74e01a4bbf2dd34c29ba6a
Dirk de Visser <dirkdev98@gmail.com>
2021-01-04 12:40:01 +0100

    cli: add test case for visualise with arguments

--- packages/cli/src/commands/visualise.test.js
+++ packages/cli/src/commands/visualise.test.js
@@ -1,19 +1,39 @@
+import { existsSync } from "fs";
 import { mainTestFn, test } from "@compas/cli";
 import { environment, spawn } from "@compas/stdlib";
 
 mainTestFn(import.meta);
 
-test("cli/commands/visualise", async (t) => {
+test("cli/commands/visualise", (t) => {
   if (environment.CI === "true") {
     t.log.info("CI detected, skipping visualise tests");
     return;
   }
-  const { exitCode } = await spawn(`yarn`, [
-    "compas",
-    "visualise",
-    "sql",
-    "./packages/store/src/generated/index.js",
-  ]);
 
-  t.equal(exitCode, 0);
+  t.test("visualise default", async (t) => {
+    const { exitCode } = await spawn(`yarn`, [
+      "compas",
+      "visualise",
+      "sql",
+      "./packages/store/src/generated/index.js",
+    ]);
+
+    t.equal(exitCode, 0);
+  });
+
+  t.test("visualise with arguments", async (t) => {
+    const { exitCode } = await spawn(`yarn`, [
+      "compas",
+      "visualise",
+      "sql",
+      "./packages/store/src/generated/index.js",
+      "--format",
+      "png",
+      "--output",
+      "/tmp/visualise.png",
+    ]);
+
+    t.equal(exitCode, 0);
+    t.ok(existsSync("/tmp/visualise.png"));
+  });
 });
