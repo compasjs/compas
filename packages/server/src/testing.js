9c8e0d97beae3ec30f7dca923021438b2b923cdd
Dirk de Visser <dirkdev98@gmail.com>
2021-02-04 19:43:46 +0100

    server: catch errors while creating and closing a test app
    Closes #655

--- packages/server/src/testing.js
+++ packages/server/src/testing.js
@@ -3,11 +3,22 @@
  * @param {AxiosInstance} axios
  */
 export async function createTestAppAndClient(app, axios) {
-  await new Promise((resolve) => {
-    app._server = app.listen(() => {
+  await new Promise((resolve, reject) => {
+    let isListening = false;
+    app._server = app.listen();
+
+    app._server.on("listening", () => {
+      isListening = true;
       resolve();
     });
+
+    app._server.on("error", (err) => {
+      if (!isListening) {
+        reject(err);
+      }
+    });
   });
+
   const { port } = app._server.address();
   axios.defaults.baseURL = `http://127.0.0.1:${port}/`;
 }
@@ -17,9 +28,15 @@ export async function createTestAppAndClient(app, axios) {
  * @returns {Promise<void>}
  */
 export function closeTestApp(app) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     if (app._server && app._server.listening) {
-      app._server.close(resolve);
+      app._server.close((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
     }
   });
 }
