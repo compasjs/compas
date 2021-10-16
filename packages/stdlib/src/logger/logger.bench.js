19c149aae24dde18ba4ee77c1a8e077e392639fa
Dirk de Visser <github@dirkdevisser.nl>
2021-10-16 15:13:06 +0200

    bench(stdlib): add better logger benchmarks

--- packages/stdlib/src/logger/logger.bench.js
+++ packages/stdlib/src/logger/logger.bench.js
@@ -1,28 +1,71 @@
-import { createWriteStream } from "fs";
 import { bench, mainBenchFn } from "@compas/cli";
+import pino from "pino";
 import { newLogger } from "./logger.js";
 
 mainBenchFn(import.meta);
 
+const getCompasCompatiblePino = () => {
+  const pinoLogger = pino(
+    {
+      formatters: {
+        level: (label) => ({ level: label }),
+        bindings: () => ({}),
+      },
+      serializers: {},
+      base: {},
+      // We don't add time here, since it's much faster without
+      // timestamp: () => `, "timestamp": "${new Date().toISOString()}"`,
+    },
+    {
+      write() {},
+    },
+  ).child({ context: { application: "compas" } });
+
+  return {
+    info: (message) => pinoLogger.info({ message }),
+    error: (message) => pinoLogger.error({ message }),
+  };
+};
+
 bench("logger - strings", (b) => {
-  const stream = createWriteStream("/tmp/logger_bench_compas.txt", "utf-8");
   const logger = newLogger({
-    pretty: false,
-    stream,
+    printer: "ndjson",
+    stream: {
+      write: () => {},
+    },
   });
 
   for (let i = 0; i < b.N; ++i) {
     logger.info("My simple string");
   }
+});
 
-  stream.end();
+bench("pino - default - strings", (b) => {
+  const logger = pino(
+    {},
+    {
+      write: () => {},
+    },
+  );
+
+  for (let i = 0; i < b.N; ++i) {
+    logger.info("My simple string");
+  }
+});
+bench("pino - compas compat - strings", (b) => {
+  const logger = getCompasCompatiblePino();
+
+  for (let i = 0; i < b.N; ++i) {
+    logger.info("My simple string");
+  }
 });
 
 bench("logger - objects", (b) => {
-  const stream = createWriteStream("/tmp/logger_bench_compas.txt", "utf-8");
   const logger = newLogger({
     printer: "ndjson",
-    stream,
+    stream: {
+      write: () => {},
+    },
   });
 
   for (let i = 0; i < b.N; ++i) {
@@ -32,15 +75,43 @@ bench("logger - objects", (b) => {
       },
     });
   }
+});
 
-  stream.end();
+bench("pino - default - objects", (b) => {
+  const logger = pino(
+    {},
+    {
+      write: () => {},
+    },
+  );
+
+  for (let i = 0; i < b.N; ++i) {
+    logger.info({
+      my: {
+        simple: "object",
+      },
+    });
+  }
+});
+
+bench("pino - compas compat - objects", (b) => {
+  const logger = getCompasCompatiblePino();
+
+  for (let i = 0; i < b.N; ++i) {
+    logger.info({
+      my: {
+        simple: "object",
+      },
+    });
+  }
 });
 
 bench("logger - deep objects", (b) => {
-  const stream = createWriteStream("/tmp/logger_bench_compas.txt", "utf-8");
   const logger = newLogger({
     printer: "ndjson",
-    stream,
+    stream: {
+      write: () => {},
+    },
   });
 
   for (let i = 0; i < b.N; ++i) {
@@ -59,6 +130,51 @@ bench("logger - deep objects", (b) => {
       bar: true,
     });
   }
+});
 
-  stream.end();
+bench("pino - default - deep objects", (b) => {
+  const logger = pino(
+    {},
+    {
+      write: () => {},
+    },
+  );
+
+  for (let i = 0; i < b.N; ++i) {
+    logger.info({
+      my: {
+        more: [
+          {
+            advanced: {
+              object: "with",
+              multiple: "keys",
+              foo: 5,
+            },
+          },
+        ],
+      },
+      bar: true,
+    });
+  }
+});
+
+bench("pino - compas compat - deep objects", (b) => {
+  const logger = getCompasCompatiblePino();
+
+  for (let i = 0; i < b.N; ++i) {
+    logger.info({
+      my: {
+        more: [
+          {
+            advanced: {
+              object: "with",
+              multiple: "keys",
+              foo: 5,
+            },
+          },
+        ],
+      },
+      bar: true,
+    });
+  }
 });
