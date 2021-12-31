38ed615accf69e5cacd586cf3cd9218540d6850a
Dirk de Visser <github@dirkdevisser.nl>
2021-12-31 15:07:43 +0100

    feat(cli): migrate bench command to new cli
    References #1306

---
+++ packages/cli/src/compas/commands/bench.d.ts
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {import("@compas/stdlib").Logger} logger
+ * @returns {Promise<import("../../cli/types.js").CliResult>}
+ */
+export function cliExecutor(
+  logger: import("@compas/stdlib").Logger,
+): Promise<import("../../cli/types.js").CliResult>;
+/**
+ * @type {import("../../generated/common/types.js").CliCommandDefinitionInput}
+ */
+export const cliDefinition: import("../../generated/common/types.js").CliCommandDefinitionInput;
+//# sourceMappingURL=bench.d.ts.map
