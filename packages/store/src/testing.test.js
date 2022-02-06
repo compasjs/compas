bef060d597deccfe42d78a35b5db73f4141b9cd6
Dirk de Visser <github@dirkdevisser.nl>
2022-02-06 10:16:46 +0100

    feat(lint-config): disable labels and optional fn params last (#1584)
    BREAKING CHANGE:
    - Changed parameter order of `createTestPostgresDatabase`, and nested `verboseSql` in to an 'object'.

--- packages/store/src/testing.test.js
+++ packages/store/src/testing.test.js
@@ -5,7 +5,7 @@ mainTestFn(import.meta);
 
 test("store/testing", (t) => {
   t.test("createTestPostgresDatabase - accepts options", async (t) => {
-    const sql = await createTestPostgresDatabase(false, { max: 3 });
+    const sql = await createTestPostgresDatabase({ max: 3 });
     await sql`SELECT 1 + 1`;
 
     t.equal(sql.options.max, 3);
