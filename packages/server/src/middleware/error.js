891122cb6bc99124a8afbc3ba7ac8a57d607472f
Daniël Hansen <hi@danielhansen.nl>
2022-06-21 12:24:22 +0200

    fix(server): ensure log level is inherited from AppError (#1868)

--- packages/server/src/middleware/error.js
+++ packages/server/src/middleware/error.js
@@ -34,8 +34,11 @@ export function errorHandler(opts) {
       let log = ctx.log.info;
 
       if (!AppError.instanceOf(error)) {
-        log = ctx.log.error;
         err = new AppError("error.server.internal", 500, {}, error);
+      }
+
+      if (error.status === 500) {
+        log = ctx.log.error;
       }
 
       ctx.status = err.status;
