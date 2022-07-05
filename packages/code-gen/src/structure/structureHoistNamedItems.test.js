644ed6de3888a6445a94493a228830abadd88a4f
Dirk de Visser <github@dirkdevisser.nl>
2022-07-05 17:12:03 +0200

    fix(code-gen): correctly prioritze existing named items in the structure (#1885)
    If a named item was copied as is in the structure, and then mutated, it would override the initial named item.

---
+++ packages/code-gen/src/structure/structureHoistNamedItems.test.js
@@ -0,0 +1,47 @@
+import { mainTestFn, test } from "@compas/cli";
+import { structureHoistNamedItems } from "./structureHoistNamedItems.js";
+
+mainTestFn(import.meta);
+
+test("code-gen/structure/structureHoistNamedItems", (t) => {
+  t.test("should not overwritten named items", (t) => {
+    const structure = {
+      group2: {
+        bar: {
+          type: "object",
+          name: "bar",
+          group: "group2",
+          isOptional: false,
+          keys: {},
+          relations: [],
+        },
+      },
+      group1: {
+        baz: {
+          type: "object",
+          name: "baz",
+          group: "group1",
+          keys: {
+            ref: {
+              type: "object",
+              keys: {},
+              relations: [],
+            },
+          },
+          relations: [],
+        },
+      },
+    };
+
+    structure.group1.baz.keys.ref = {
+      ...structure.group2.bar,
+      isOptional: true,
+    };
+
+    t.log.info(structure);
+
+    structureHoistNamedItems(structure);
+
+    t.equal(structure.group2.bar.isOptional, false);
+  });
+});
