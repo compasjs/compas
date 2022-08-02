6f6604c03837b5b3d5f2aac4af82bcfb5f6251f7
Dirk de Visser <github@dirkdevisser.nl>
2022-08-02 11:05:22 +0200

    feat(store): replace `file` and `minio` with `objectStorage` and `objectStore`
    Closes #1906

---
+++ packages/store/src/object-storage.js
@@ -0,0 +1,218 @@
+import {
+  CreateBucketCommand,
+  DeleteBucketCommand,
+  DeleteObjectsCommand,
+  GetObjectCommand,
+  HeadBucketCommand,
+  paginateListObjectsV2,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { AppError, isNil } from "@compas/stdlib";
+
+/**
+ * Get the development config that works with the default `compas docker up` created
+ * Minio container.
+ *
+ * Only use this container and default config when `!isProduction()`!
+ *
+ * @returns {Partial<import("@aws-sdk/client-s3").S3ClientConfig>}
+ */
+export function objectStorageGetDevelopmentConfig() {
+  return {
+    credentials: {
+      accessKeyId: "minio",
+      secretAccessKey: "minio123",
+    },
+    endpoint: "http://127.0.0.1:9000",
+    forcePathStyle: true,
+  };
+}
+
+/**
+ * Create a new S3Client.
+ *
+ * @param {import("@aws-sdk/client-s3").S3ClientConfig} config
+ * @returns {import("@aws-sdk/client-s3").S3Client}
+ */
+export function objectStorageCreateClient(config) {
+  return new S3Client(config);
+}
+
+/**
+ * Check if the supplied bucketName exists, else create it in the (optional) location.
+ *
+ * @param {import("@aws-sdk/client-s3").S3Client} s3Client
+ * @param {{
+ *   bucketName: string,
+ *   locationConstraint: import("@aws-sdk/client-s3").BucketLocationConstraint,
+ * }} options
+ * @returns {Promise<void>}
+ */
+export async function objectStorageEnsureBucket(s3Client, options) {
+  if (isNil(options?.bucketName)) {
+    throw AppError.serverError({
+      message: "Parameter 'options.bucketName' is required.",
+    });
+  }
+
+  try {
+    const headBucketResult = await s3Client.send(
+      new HeadBucketCommand({ Bucket: options.bucketName }),
+    );
+
+    if (headBucketResult.$metadata.httpStatusCode === 200) {
+      return;
+    }
+  } catch (e) {
+    // @ts-expect-error
+    if (e.name !== "NotFound") {
+      throw AppError.serverError(
+        {
+          message: "Could not create S3 bucket",
+        }, // @ts-expect-error
+        e,
+      );
+    }
+  }
+
+  const createBucketResult = await s3Client.send(
+    new CreateBucketCommand({
+      Bucket: options.bucketName,
+      ACL: "private",
+      CreateBucketConfiguration: {
+        LocationConstraint: options.locationConstraint,
+      },
+    }),
+  );
+
+  if (createBucketResult.$metadata.httpStatusCode !== 200) {
+    throw AppError.serverError({
+      message: `Could not create the '${options.bucketName}' bucket.`,
+      metadata: createBucketResult.$metadata,
+    });
+  }
+}
+
+/**
+ * Remove a bucket. Note that a bucket should be empty before it can be removed.
+ * Pass `options.includeAllObjects` to remove any left over object in the bucket before
+ * removing the bucket.
+ *
+ * @param {import("@aws-sdk/client-s3").S3Client} s3Client
+ * @param {{
+ *   bucketName: string,
+ *   includeAllObjects?: boolean,
+ * }} options
+ * @returns {Promise<void>}
+ */
+export async function objectStorageRemoveBucket(s3Client, options) {
+  if (isNil(options?.bucketName)) {
+    throw AppError.serverError({
+      message: "Parameter 'options.bucketName' is required.",
+    });
+  }
+
+  if (options.includeAllObjects === true) {
+    // The max page size of listObjectsV2 is the same as the max amount of keys that can
+    // be sent to deleteObjects, so we can map the result directly to the delete request.
+    for await (const partial of objectStorageListObjects(s3Client, {
+      bucketName: options.bucketName,
+    })) {
+      if (isNil(partial.Contents)) {
+        continue;
+      }
+
+      await s3Client.send(
+        new DeleteObjectsCommand({
+          Bucket: options.bucketName,
+          Delete: {
+            Objects: partial.Contents.map((it) => ({ Key: it.Key })),
+            Quiet: true,
+          },
+        }),
+      );
+    }
+  }
+
+  await s3Client.send(
+    new DeleteBucketCommand({
+      Bucket: options.bucketName,
+    }),
+  );
+}
+
+/**
+ * Creates a listObjectsV2 async iterator.
+ * Can be used like:
+ * ```
+ * for await (const objects of objectStorageListObjects(s3Client, { bucketName }) {
+ *   // your code using `objects.Contents`;
+ * }
+ * ```
+ *
+ * @param {import("@aws-sdk/client-s3").S3Client} s3Client
+ * @param {{
+ *   bucketName: string,
+ * }} options
+ * @returns {ReturnType<typeof import("@aws-sdk/client-s3").paginateListObjectsV2>}
+ */
+export function objectStorageListObjects(s3Client, options) {
+  if (isNil(options?.bucketName)) {
+    throw AppError.serverError({
+      message: "Parameter 'options.bucketName' is required.",
+    });
+  }
+
+  return paginateListObjectsV2(
+    {
+      client: s3Client,
+    },
+    {
+      Bucket: options.bucketName,
+    },
+  );
+}
+
+/**
+ * Get a Readable stream for the specified `bucketName` and `objectKey` combination.
+ * Normally, the objectKey is the same as `StoreFile#id`. You can also provide an
+ * optional range, to support HTTP range requests, mostly used for video players.
+ *
+ * @param {import("@aws-sdk/client-s3").S3Client} s3Client
+ * @param {{
+ *   bucketName: string,
+ *   objectKey: string,
+ *   range?: {
+ *     start?: number,
+ *     end?: number,
+ *   },
+ * }} options
+ * @returns {Promise<NodeJS.ReadableStream>}
+ */
+export async function objectStorageGetObjectStream(s3Client, options) {
+  if (isNil(options?.bucketName)) {
+    throw AppError.serverError({
+      message: "Parameter 'options.bucketName' is required.",
+    });
+  }
+  if (isNil(options?.objectKey)) {
+    throw AppError.serverError({
+      message: "Parameter 'options.objectKey' is required.",
+    });
+  }
+
+  let range = undefined;
+  if (options.range) {
+    range = `bytes=${options.range.start ?? 0}-${options.range.end ?? ""}`;
+  }
+
+  const getObjectResult = await s3Client.send(
+    new GetObjectCommand({
+      Bucket: options.bucketName,
+      Key: options.objectKey,
+      Range: range,
+    }),
+  );
+
+  return getObjectResult.Body;
+}
---
+++ packages/store/src/object-storage.test.js
@@ -0,0 +1,327 @@
+import {
+  HeadBucketCommand,
+  ListBucketsCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import { mainTestFn, test } from "@compas/cli";
+import { isNil, streamToBuffer, uuid } from "@compas/stdlib";
+import {
+  objectStorageCreateClient,
+  objectStorageEnsureBucket,
+  objectStorageGetDevelopmentConfig,
+  objectStorageGetObjectStream,
+  objectStorageListObjects,
+  objectStorageRemoveBucket,
+} from "./object-storage.js";
+
+mainTestFn(import.meta);
+
+test("store/object-storage", (t) => {
+  // TODO: appError format of an S3 error...
+
+  t.test("objectStorageGetDevelopmentConfig", (t) => {
+    const config = objectStorageGetDevelopmentConfig();
+    const secondConfig = objectStorageGetDevelopmentConfig();
+
+    t.deepEqual(config, secondConfig);
+  });
+
+  t.test("objectStorageCreateClient", (t) => {
+    const client = objectStorageCreateClient(
+      objectStorageGetDevelopmentConfig(),
+    );
+
+    t.ok(client);
+  });
+
+  t.test("objectStorageEnsureBucket", (t) => {
+    const bucketName = uuid();
+
+    t.test("bucketName is required", async (t) => {
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+
+      try {
+        await objectStorageEnsureBucket(client, {});
+      } catch (e) {
+        t.ok(e.info.message.includes("required"));
+      }
+    });
+
+    t.test("bucket is created", async (t) => {
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+
+      await objectStorageEnsureBucket(client, {
+        bucketName,
+      });
+
+      const headCommandResult = await client.send(
+        new HeadBucketCommand({
+          Bucket: bucketName,
+        }),
+      );
+      t.equal(headCommandResult.$metadata.httpStatusCode, 200);
+    });
+
+    t.test("bucket is created - with location", async (t) => {
+      const bucketName = uuid();
+      const location = "eu-central-1";
+
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+
+      await objectStorageEnsureBucket(client, {
+        bucketName,
+        locationConstraint: location,
+      });
+
+      const headCommandResult = await client.send(
+        new HeadBucketCommand({
+          Bucket: bucketName,
+        }),
+      );
+      t.equal(headCommandResult.$metadata.httpStatusCode, 200);
+    });
+
+    t.test("bucket exists", async (t) => {
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+
+      await objectStorageEnsureBucket(client, {
+        bucketName,
+      });
+
+      const headCommandResult = await client.send(
+        new HeadBucketCommand({
+          Bucket: bucketName,
+        }),
+      );
+      t.equal(headCommandResult.$metadata.httpStatusCode, 200);
+    });
+  });
+
+  t.test("objectStorageRemoveBucket", (t) => {
+    t.test("bucketName is required", async (t) => {
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+
+      try {
+        await objectStorageRemoveBucket(client, {});
+      } catch (e) {
+        t.ok(e.info.message.includes("required"));
+      }
+    });
+
+    t.test("empty bucket", async (t) => {
+      const bucketName = uuid();
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+      await objectStorageEnsureBucket(client, {
+        bucketName,
+      });
+      await objectStorageRemoveBucket(client, {
+        bucketName,
+      });
+
+      const listBucketsResult = await client.send(new ListBucketsCommand({}));
+
+      t.ok(
+        isNil(listBucketsResult.Buckets.find((it) => it.Name === bucketName)),
+      );
+    });
+
+    t.test("bucket not empty error", async (t) => {
+      const bucketName = uuid();
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+      await objectStorageEnsureBucket(client, {
+        bucketName,
+      });
+
+      await client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: uuid(),
+          Body: Buffer.from("foo"),
+          ContentType: "text/plain",
+        }),
+      );
+
+      try {
+        await objectStorageRemoveBucket(client, {
+          bucketName,
+        });
+      } catch (e) {
+        t.equal(e.name, "BucketNotEmpty");
+      }
+    });
+
+    t.test("bucket not empty - delete all objects", async (t) => {
+      const bucketName = uuid();
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+      await objectStorageEnsureBucket(client, {
+        bucketName,
+      });
+
+      await client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: uuid(),
+          Body: Buffer.from("foo"),
+          ContentType: "text/plain",
+        }),
+      );
+
+      await objectStorageRemoveBucket(client, {
+        bucketName,
+        includeAllObjects: true,
+      });
+
+      const listBucketsResult = await client.send(new ListBucketsCommand({}));
+
+      t.ok(
+        isNil(listBucketsResult.Buckets.find((it) => it.Name === bucketName)),
+      );
+    });
+  });
+
+  t.test("objectStorageListObjects", (t) => {
+    t.test("bucketName is required", async (t) => {
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+
+      try {
+        await objectStorageListObjects(client, {});
+      } catch (e) {
+        t.ok(e.info.message.includes("required"));
+      }
+    });
+
+    t.test("iterator is returned", async (t) => {
+      const bucketName = uuid();
+      const objectKey = uuid();
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+      await objectStorageEnsureBucket(client, {
+        bucketName,
+      });
+
+      await client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: objectKey,
+          Body: Buffer.from("foo"),
+          ContentType: "text/plain",
+        }),
+      );
+
+      for await (const result of objectStorageListObjects(client, {
+        bucketName,
+      })) {
+        t.equal(result.Contents[0].Key, objectKey);
+      }
+    });
+  });
+
+  t.test("objectStorageGetObjectStream", (t) => {
+    t.test("bucketName is required", async (t) => {
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+
+      try {
+        await objectStorageGetObjectStream(client, {});
+      } catch (e) {
+        t.ok(e.info.message.includes("required"));
+      }
+    });
+
+    t.test("objectKey is required", async (t) => {
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+
+      try {
+        await objectStorageGetObjectStream(client, {
+          bucketName: uuid(),
+        });
+      } catch (e) {
+        t.ok(e.info.message.includes("required"));
+      }
+    });
+
+    t.test("success", async (t) => {
+      const bucketName = uuid();
+      const objectKey = uuid();
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+      await objectStorageEnsureBucket(client, {
+        bucketName,
+      });
+
+      await client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: objectKey,
+          Body: Buffer.from("foo"),
+          ContentType: "text/plain",
+        }),
+      );
+
+      const buffer = await streamToBuffer(
+        await objectStorageGetObjectStream(client, {
+          bucketName,
+          objectKey,
+        }),
+      );
+
+      t.equal(buffer.toString("utf-8"), "foo");
+    });
+
+    t.test("success - with range", async (t) => {
+      const bucketName = uuid();
+      const objectKey = uuid();
+      const client = objectStorageCreateClient(
+        objectStorageGetDevelopmentConfig(),
+      );
+      await objectStorageEnsureBucket(client, {
+        bucketName,
+      });
+
+      await client.send(
+        new PutObjectCommand({
+          Bucket: bucketName,
+          Key: objectKey,
+          Body: Buffer.from("foo"),
+          ContentType: "text/plain",
+        }),
+      );
+
+      const buffer = await streamToBuffer(
+        await objectStorageGetObjectStream(client, {
+          bucketName,
+          objectKey,
+          range: {
+            start: 0,
+            end: 1,
+          },
+        }),
+      );
+
+      t.equal(buffer.toString("utf-8"), "fo");
+    });
+  });
+});
