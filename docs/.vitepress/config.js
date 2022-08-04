7eacf849b2324096a19eed89da0b37ac2d8cb3ad
Dirk de Visser <github@dirkdevisser.nl>
2022-08-04 15:53:33 +0200

    docs: add release notes with breaking changes

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -205,6 +205,7 @@ function getHomeSidebar() {
 
 function getReleaseNotesSidebar() {
   return [
+    { text: "Release v0.0.212", link: "/releases/0.0.212.html" },
     { text: "Release v0.0.180", link: "/releases/0.0.180.html" },
     { text: "Release v0.0.172", link: "/releases/0.0.172.html" },
     { text: "Release v0.0.171", link: "/releases/0.0.171.html" },
