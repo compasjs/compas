9e299cd3d6b46c16f3a8c1962ab608b80e6d031a
Dirk de Visser <github@dirkdevisser.nl>
2022-08-06 22:50:29 +0200

    feat(create-compas): add args parser
    Closes #1965

--- packages/create-compas/src/create-compas.js
+++ packages/create-compas/src/create-compas.js
@@ -1,12 +1,46 @@
 #! /usr/bin/env node
 
-import { mainFn } from "@compas/stdlib";
+import { readFile } from "fs/promises";
+import { dirnameForModule, mainFn, pathJoin } from "@compas/stdlib";
+import {
+  argParserParse,
+  argParserValidate,
+  createCompasFlags,
+} from "./arg-parser.js";
+import { helpPrintCreateCompasHelp } from "./help.js";
 
 mainFn(import.meta, main);
 
-function main(logger) {
+async function main(logger) {
+  const args = process.argv.slice(2);
+  const parsedArgs = argParserParse(createCompasFlags, args);
+
+  if (parsedArgs.error) {
+    logger.error(parsedArgs.error.message);
+    process.exit(1);
+
+    return;
+  }
+
+  const { version: createCompasVersion } = JSON.parse(
+    await readFile(
+      pathJoin(dirnameForModule(import.meta), "../package.json"),
+      "utf-8",
+    ),
+  );
+  const validatedArgs = argParserValidate(
+    parsedArgs.value,
+    `v${createCompasVersion}`,
+  );
+
+  if (validatedArgs.help) {
+    helpPrintCreateCompasHelp(logger, validatedArgs.message);
+    return;
+  }
+
   logger.info(`
 Create Compas based projects from the examples or custom templates.
+Resolved template url: 'https://github.com/${validatedArgs.template.repository}/tree/${validatedArgs.template.ref}/${validatedArgs.template.path}'
 
 This package is a work in progress. Follow https://github.com/compasjs/compas/issues/1907 for updates.`);
 }
