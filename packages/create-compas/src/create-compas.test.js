6a8e04d9ab5d3d269d12271e9d34c1f5e8baf4e3
Dirk de Visser <github@dirkdevisser.nl>
2022-08-07 10:33:35 +0200

    feat(create-compas): determine and check the output directory (#1971)
    Closes #1966

--- packages/create-compas/src/create-compas.test.js
+++ packages/create-compas/src/create-compas.test.js
@@ -4,7 +4,9 @@ import { exec } from "@compas/stdlib";
 mainTestFn(import.meta);
 
 test("create-compas", async (t) => {
-  const { exitCode, stdout } = await exec("create-compas");
+  const { exitCode, stdout } = await exec(
+    "create-compas --output-directory ./compas-project",
+  );
 
   t.equal(exitCode, 0);
 
