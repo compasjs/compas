bb64c93cc6e3db976760dbaf6278008a940abd1a
dirkdev98 <dirkdev98@users.noreply.github.com>
2022-08-07 08:34:37 +0000

    chore(sync): sync generated doc files

--- packages/create-compas/src/create-compas.d.ts
+++ packages/create-compas/src/create-compas.d.ts
@@ -1,3 +1,10 @@
 #! /usr/bin/env node
-export {};
+/**
+ * Check if the provided directory can be used. Ignoring some existing configuration
+ * files.
+ *
+ * @param {string} dir
+ * @returns {boolean}
+ */
+export function canDirectoryBeUsed(dir: string): boolean;
 //# sourceMappingURL=create-compas.d.ts.map
