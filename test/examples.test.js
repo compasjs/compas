4a06337dfad5742f1bab0defd9f3b860645648e6
Dirk de Visser <github@dirkdevisser.nl>
2023-05-09 23:09:41 +0200

    chore(test): make sure the custom test timeout applies by running example tests in a subtest

--- test/examples.test.js
+++ test/examples.test.js
@@ -31,38 +31,40 @@ test("compas/examples", async (t) => {
     )
   ).filter((it) => !!it);
 
-  await Promise.all(
-    configs.map(async (config) => {
-      for (const cmd of config.exampleMetadata.testing) {
-        const parts = cmd.split(" ");
-        if (parts[0] === "compas") {
-          parts[0] = "../../node_modules/.bin/compas";
-        }
+  t.test("run examples", async (t) => {
+    await Promise.all(
+      configs.map(async (config) => {
+        for (const cmd of config.exampleMetadata.testing) {
+          const parts = cmd.split(" ");
+          if (parts[0] === "compas") {
+            parts[0] = "../../node_modules/.bin/compas";
+          }
 
-        const { exitCode, stdout, stderr } = await exec(parts.join(" "), {
-          cwd: config.exampleMetadata.path,
-          env: {
-            PATH: environment.PATH,
-            CI: environment.CI,
-            GITHUB_ACTIONS: environment.GITHUB_ACTIONS,
-          },
-        });
-
-        t.equal(
-          exitCode,
-          0,
-          `Path: ${config.exampleMetadata.path}, Command: ${cmd}, Exitcode: ${exitCode}`,
-        );
-
-        if (exitCode !== 0) {
-          t.log.error({
-            config,
-            cmd,
-            stdout,
-            stderr,
+          const { exitCode, stdout, stderr } = await exec(parts.join(" "), {
+            cwd: config.exampleMetadata.path,
+            env: {
+              PATH: environment.PATH,
+              CI: environment.CI,
+              GITHUB_ACTIONS: environment.GITHUB_ACTIONS,
+            },
           });
+
+          t.equal(
+            exitCode,
+            0,
+            `Path: ${config.exampleMetadata.path}, Command: ${cmd}, Exitcode: ${exitCode}`,
+          );
+
+          if (exitCode !== 0) {
+            t.log.error({
+              config,
+              cmd,
+              stdout,
+              stderr,
+            });
+          }
         }
-      }
-    }),
-  );
+      }),
+    );
+  });
 });
