c80410cddb6529f9e6dfa376994e65de5e98114c
Dirk de Visser <github@dirkdevisser.nl>
2023-07-31 15:36:55 +0200

    chore: cleanup code-mods

--- packages/cli/src/code-mod/constants.js
+++ packages/cli/src/code-mod/constants.js
@@ -1,7 +1,4 @@
 import { cpus } from "node:os";
-import { executeApiClientToExperimentalCodeGen } from "./mods/api-client-to-experimental-code-gen.js";
-import { executeLintConfigToEslintPlugin } from "./mods/lint-config-to-eslint-plugin.js";
-import { executeUpdateQueriesSignatureChange } from "./mods/update-queries-signature-change.js";
 
 export const PARALLEL_COUNT = Math.max(cpus().length - 1, 1);
 
@@ -11,72 +8,4 @@ export const PARALLEL_COUNT = Math.max(cpus().length - 1, 1);
  *    exec: (logger: Logger) => Promise<void>
  * }>}
  */
-export const codeModMap = {
-  "update-queries-signature-change": {
-    description: `Convert arguments in call sites of generated 'queries.entityUpdate' to pass in the named 'where' and 'update' parameters.
-  It also adds 'returning: "*"' if it detects that the result is used, or if it is unable to detect that the return value is unused.
-
-  Inline update & where;
-    // old
-    await queries.entityUpdate(sql, { /* inline update */ }, { /* inline where  */});
-    // new
-    await queries.entityUpdate(sql, {
-      update: { /* inline update */ },
-      where: { /* inline where */ },
-    });
-
-  Referenced update & where:
-    // old
-    await queries.entityUpdate(sql, update, where);
-    // new
-    await queries.entityUpdate(sql, {
-      update,
-      where,
-    });
-
-  Infer that result is used:
-    // old
-    const [updatedEntity] = await queries.entityUpdate(sql, update, where);
-    // new
-    const [updatedEntity] = await queries.entityUpdate(sql, {
-      update,
-      where,
-      returning: "*",
-    });
-
-  Or any combination of the above.
-`,
-    exec: executeUpdateQueriesSignatureChange,
-  },
-  "lint-config-to-eslint-plugin": {
-    description: `Convert all known usages of @compas/lint-config to use @compas/eslint-plugin.
-
-  This only updates the configuration files and does not update the code to be consistent with the newly enforced rules.
-`,
-    exec: executeLintConfigToEslintPlugin,
-  },
-  "api-client-to-experimental-code-gen": {
-    description: `Convert the project to use experimental code-gen based on a list of structures in '$project/structures.txt'.
-
-  'structures.txt' has the following format;
-     https://a.remote.compas.backend -- src/generated
-     ./local-openapi.json -- src/generated/foo -- defaultGroup
-     
-  The code-mode executes the following steps:
-    - Resolve and validated 'structures.txt'
-    - Resolve all mentioned structures from 'structures.txt'
-    - Overwrite 'scripts/generate.mjs'
-    - Execute 'scripts/generate.mjs'
-    - Try to overwrite as much type usages as possible based on the cleaner type name generation.
-    
-  Manual cleanup:
-    - Remove structures.txt
-    - Copy-edit & cleanup 'scripts/generate.mjs'
-      - Use environment variables where appropriate
-      - Cleanup imports
-      - Correct 'targetRuntime' when using React-native.
-    - Go through 'mutation' hooks usage & flatten arguments
-    `,
-    exec: executeApiClientToExperimentalCodeGen,
-  },
-};
+export const codeModMap = {};
--- packages/cli/src/code-mod/mods/lint-config-to-eslint-plugin.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Convert all known usages from @compas/lint-config to eslint-plugin
- *
- * @param {Logger} logger
- * @returns {Promise<void>}
- */
-export function executeLintConfigToEslintPlugin(logger: Logger): Promise<void>;
-//# sourceMappingURL=lint-config-to-eslint-plugin.d.ts.map
