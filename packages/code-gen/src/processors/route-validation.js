213e6be9fd462b248ff72476df84367ad1e0831c
Dirk de Visser <github@dirkdevisser.nl>
2024-07-11 11:44:40 +0200

    build(deps-dev): bump typescript from 5.4.5 to 5.5.3

--- packages/code-gen/src/processors/route-validation.js
+++ packages/code-gen/src/processors/route-validation.js
@@ -46,8 +46,6 @@ function routeValidationSimpleQueryAndParamTypes(
 
   const resolvedType = structureResolveReference(
     generateContext.structure,
-
-    // @ts-expect-error
     route[subType],
   );
 
