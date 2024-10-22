129de8e9e7a51681c836455db7c311ab71799b9d
Dirk de Visser <github@dirkdevisser.nl>
2024-10-22 15:58:09 +0200

    feat(cli): optional `t` argument for `newTestEvent`

--- packages/cli/src/testing/events.js
+++ packages/cli/src/testing/events.js
@@ -1,13 +1,23 @@
-import { newEvent } from "@compas/stdlib";
+import { isNil, newEvent, newLogger } from "@compas/stdlib";
 
 /**
  * Create a new test event
  *
  * @since 0.1.0
  *
- * @param {import("../../types/advanced-types.d.ts").TestRunner} t
+ * @param {import("../../types/advanced-types.d.ts").TestRunner} [t]
  * @returns {import("@compas/stdlib").InsightEvent}
  */
 export function newTestEvent(t) {
-  return newEvent(t.log, t.signal);
+  if (!isNil(t) && typeof t === "object" && typeof t.log?.info === "function") {
+    return newEvent(t.log, t.signal);
+  }
+
+  return newEvent(
+    newLogger({
+      ctx: {
+        type: "new-test-event",
+      },
+    }),
+  );
 }
