f31ef026a6f904e93c7f178f47459cd175ca1654
Dirk de Visser <github@dirkdevisser.nl>
2025-02-17 16:50:40 +0100

    fix(store): query resolver now handles one-to-many expansions in optional joins
    Also keeps or adds the `?` modifier on optional entity fields when mapping the types.

--- packages/store/types/advanced-types.d.ts
+++ packages/store/types/advanced-types.d.ts
@@ -2,77 +2,76 @@ import { Postgres } from "packages/store/index";
 import type { PendingQuery } from "postgres";
 
 export type QueryPartArg =
-	| string
-	| boolean
-	| number
-	| null
-	| undefined
-	| Date
-	| QueryPart
-	| QueryPartArg[];
+  | string
+  | boolean
+  | number
+  | null
+  | undefined
+  | Date
+  | QueryPart
+  | QueryPartArg[];
 
 export interface QueryPart<T = any> {
-	get strings(): string[];
+  get strings(): string[];
 
-	get values(): QueryPartArg[];
+  get values(): QueryPartArg[];
 
-	append(query: QueryPart): QueryPart<T>;
+  append(query: QueryPart): QueryPart<T>;
 
-	exec(sql: Postgres): PendingQuery<T>;
+  exec(sql: Postgres): PendingQuery<T>;
 }
 
 /**
  * Wrap query builder results
  */
 export type WrappedQueryPart<Type = any> = {
-	/**
-	 * Get the underlying queryPart, representing the build query
-	 */
-	queryPart: QueryPart;
+  /**
+   * Get the underlying queryPart, representing the build query
+   */
+  queryPart: QueryPart;
 
-	/**
-	 * @deprecated Use `.exec` or `.execRaw`
-	 */
-	then(): never;
+  /**
+   * @deprecated Use `.exec` or `.execRaw`
+   */
+  then(): never;
 
-	/**
-	 * Exec the query and return validated query results.
-	 */
-	exec(sql: Postgres): Promise<Type[]>;
+  /**
+   * Exec the query and return validated query results.
+   */
+  exec(sql: Postgres): Promise<Type[]>;
 
-	/**
-	 * Exec the query and return raw query results. This should be used when a custom 'select'
-	 * or 'returning' is used.
-	 */
-	execRaw(sql: Postgres): Promise<Partial<Type>[]>;
+  /**
+   * Exec the query and return raw query results. This should be used when a custom 'select'
+   * or 'returning' is used.
+   */
+  execRaw(sql: Postgres): Promise<Partial<Type>[]>;
 };
 
 /**
  * Wrap query builder results
  */
 export type WrappedQueryResult<Type = any> = {
-	/**
-	 * Get the underlying queryPart, representing the build query
-	 */
-	queryPart: QueryPart;
+  /**
+   * Get the underlying queryPart, representing the build query
+   */
+  queryPart: QueryPart;
 
-	/**
-	 * @deprecated Use `.exec` or `.execRaw`
-	 */
-	then(): never;
+  /**
+   * @deprecated Use `.exec` or `.execRaw`
+   */
+  then(): never;
 
-	/**
-	 * Exec the query and return validated query results.
-	 */
-	exec(sql: Postgres): Promise<Type[]>;
+  /**
+   * Exec the query and return validated query results.
+   */
+  exec(sql: Postgres): Promise<Type[]>;
 
-	/**
-	 * Exec the query and return raw query results. This should be used when a custom 'select'
-	 * or 'returning' is used.
-	 */
-	execRaw(sql: Postgres): Promise<Type[]>;
+  /**
+   * Exec the query and return raw query results. This should be used when a custom 'select'
+   * or 'returning' is used.
+   */
+  execRaw(sql: Postgres): Promise<Type[]>;
 };
-
 
 // ------ QueryBuilder Type Resolver ------
 
@@ -86,29 +85,39 @@ type _ResolveType<T> = { [K in keyof T]: T[K] } & {};
  * Utility type to resolve the base + expansion of an entity.
  */
 export type QueryBuilderDefinition<Base, Expansion> = {
-	base: Base;
-	expansion: Expansion;
+  base: Base;
+  expansion: Expansion;
 };
 
 type PickKeysThatExtend<T, Select> = {
-	[K in keyof T as T[K] extends Select ? K : never]: T[K];
+  [K in keyof T as T[K] extends Select ? K : never]: T[K];
 };
 
 type OmitKeysThatExtend<T, Select> = {
-	[K in keyof T as T[K] extends Select ? never : K]: T[K];
+  [K in keyof T as T[K] extends Select ? never : K]: T[K];
 };
 
-type ConvertNeverAndUndefined<T> =
-	OmitKeysThatExtend<OmitKeysThatExtend<T, never>, undefined>
-	& Partial<PickKeysThatExtend<OmitKeysThatExtend<T, never>, undefined>>;
+/**
+ * Omit never values
+ */
+type OmitNever<T> = OmitKeysThatExtend<T, never>;
+
+/**
+ * Apply Partial if the type can be undefined.
+ */
+type ConvertUndefinedToPartial<T> = {
+	[K in keyof T as undefined extends T[K] ? K : never] ?: T[K];
+} & {
+	[K in keyof T as undefined extends T[K] ? never : K]: T[K];
+};
 
 type QueryBuilderSpecialKeys =
-	| "offset"
-	| "limit"
-	| "orderBy"
-	| "orderBySpec"
-	| "select"
-	| "where";
+  | "offset"
+  | "limit"
+  | "orderBy"
+  | "orderBySpec"
+  | "select"
+  | "where";
 
 /**
  * Max value for which optional joins are resolved.
@@ -120,62 +129,66 @@ type ResolveJoinDepth = 6;
  * until {@link ResolveJoinDepth} depth.
  */
 export type ResolveOptionalJoins<
-	Expansion,
-	Prefix extends string = "",
-	Depth extends Array<unknown> = [],
+  Expansion,
+  Prefix extends string = "",
+  Depth extends Array<unknown> = [],
 > = Depth["length"] extends ResolveJoinDepth
-		? never
-		: Expansion extends object
-			? {
-				[K in keyof Expansion]: K extends string
-																? Expansion[K] extends {
-																								 expansion: unknown;
-																							 }
-																	? Prefix extends "" // Base case
-																		?
-																		| `${K}`
-																		| ResolveOptionalJoins<
-																			Expansion[K]["expansion"],
-																			`${K}`,
-																			[unknown, ...Depth]
-																		>
-																		: // Nested case
-																		| `${Prefix}.${K}` // Recursive into other expansions.
-																		| ResolveOptionalJoins<
-																			Expansion[K]["expansion"],
-																			`${Prefix}.${K}`,
-																			[unknown, ...Depth]
-																		>
-																	: never
-																: never;
-			}[keyof Expansion]
-			: never;
+  ? never
+  : Expansion extends {
+        expansion: unknown;
+      }
+    ? ResolveOptionalJoins<Expansion["expansion"], Prefix, Depth>
+    : Expansion extends Array<{
+          expansion: unknown;
+        }>
+      ? ResolveOptionalJoins<Expansion[0]["expansion"], Prefix, Depth>
+      : Expansion extends object
+        ? {
+            [K in keyof Expansion]: K extends string
+              ? Prefix extends "" // Base case
+                ?
+                    | `${K}`
+                    | ResolveOptionalJoins<
+                        Expansion[K],
+                        `${K}`,
+                        [unknown, ...Depth]
+                      >
+                : // Nested case
+                  | `${Prefix}.${K}` // Recursive into other expansions.
+                    | ResolveOptionalJoins<
+                        Expansion[K],
+                        `${Prefix}.${K}`,
+                        [unknown, ...Depth]
+                      >
+              : never;
+          }[keyof Expansion]
+        : never;
 
 /**
  * Split the input string on the first '.'-char.
  */
 type SplitDot<Input extends string> = Input extends `${infer Start}.${string}`
-																			? Start
-																			: never;
+  ? Start
+  : never;
 
 /**
  * Check if the provided key is in one of the optional joins. This is also true when the key
  * is a prefix of a join. i.e `settings` is optional if `settings.user` is an optional join.
  */
 type IsOptionalJoin<
-	Key extends string,
-	Joins extends string,
+  Key extends string,
+  Joins extends string,
 > = Key extends Joins ? true : Key extends SplitDot<Joins> ? true : false;
 
 /**
  * Filters and strips the Joins that start with Prefix.
  */
 type FilterOptionalJoins<Joins extends string, Prefix extends string> = {
-	[K in Joins]: K extends `${Prefix}`
-								? never
-								: K extends `${Prefix}.${infer Suffix}`
-									? Suffix
-									: never;
+  [K in Joins]: K extends `${Prefix}`
+    ? never
+    : K extends `${Prefix}.${infer Suffix}`
+      ? Suffix
+      : never;
 }[Joins];
 
 /**
@@ -183,56 +196,74 @@ type FilterOptionalJoins<Joins extends string, Prefix extends string> = {
  * If no select field exists on the builder, or if "*" is supplied, the full Type is returned.
  */
 type PickSelected<Type, SelectBuilder> = SelectBuilder extends {
-																												 select: "*" | Array<string>;
-																											 }
-																				 ? SelectBuilder["select"] extends "*" // Select all fields
-																					 ? Type
-																					 : SelectBuilder["select"] extends Array<infer K extends keyof Type>
-																						 ? // Only select the fields that have been selected.
-																						 Pick<Type, K>
-																						 : never
-																				 : // Defaults to selecting all fields.
-																				 Type;
+  select: "*" | Array<string>;
+}
+  ? SelectBuilder["select"] extends "*" // Select all fields
+    ? Type
+    : SelectBuilder["select"] extends Array<infer K extends keyof Type> // Only select the fields
+      ? // that have been selected.
+        Pick<Type, K>
+      : never // Defaults to selecting all fields.
+  :
+    Type;
 
-type ResolveBaseResult<Base, QueryBuilder, OptionalJoins extends string> = PickSelected<
-	Omit<
-		Base,
-		Exclude<keyof QueryBuilder, QueryBuilderSpecialKeys> | OptionalJoins
-	>,
-	QueryBuilder
->
+type ResolveBaseResult<
+  Base,
+  QueryBuilder,
+  OptionalJoins extends string,
+> = OmitNever<
+	ConvertUndefinedToPartial<
+		PickSelected<
+  		Omit<
+    		Base,
+    		Exclude<keyof QueryBuilder, QueryBuilderSpecialKeys> | OptionalJoins
+  		>,
+  		QueryBuilder
+		>
+	>
+>;
 
-type ResolveTypeFromExpansion<DefinitionType, QueryBuilder, OptionalJoins extends string> = DefinitionType extends Array<infer SingleDefinition>
-																																														? Array<QueryBuilderResolver<SingleDefinition, QueryBuilder, OptionalJoins>>
-																																														: QueryBuilderResolver<DefinitionType, QueryBuilder, OptionalJoins>;
+type ResolveTypeFromExpansion<
+  DefinitionType,
+  QueryBuilder,
+  OptionalJoins extends string,
+> =
+  DefinitionType extends Array<infer SingleDefinition>
+    ? Array<QueryBuilderResolver<SingleDefinition, QueryBuilder, OptionalJoins>>
+    : QueryBuilderResolver<DefinitionType, QueryBuilder, OptionalJoins>;
 
 type ResolveExpansionKey<
-	K extends (keyof Expansion & string),
-	Base, Expansion, QueryBuilder,
-	OptionalJoins extends string
+  K extends keyof Expansion & string,
+  Base,
+  Expansion,
+  QueryBuilder,
+  OptionalJoins extends string,
 > = K extends keyof QueryBuilder
-		? ResolveTypeFromExpansion<
-		Expansion[K],
-		QueryBuilder[K],
-		FilterOptionalJoins<OptionalJoins, K>
-	>
-		:
-		IsOptionalJoin<K, OptionalJoins> extends true ?
-		K extends keyof Base ?
-		// We need to include the base type if it exists for owning sides of relations.
-		| ResolveTypeFromExpansion<
-			Expansion[K],
-			unknown,
-			FilterOptionalJoins<OptionalJoins, K>
-		> | Base[K] | undefined
-												 :
-		| ResolveTypeFromExpansion<
-			Expansion[K],
-			unknown,
-			FilterOptionalJoins<OptionalJoins, K>
-		>
-		| undefined
-	: K extends keyof Base ? Base[K] : never;
+  ? ResolveTypeFromExpansion<
+      Expansion[K],
+      QueryBuilder[K],
+      FilterOptionalJoins<OptionalJoins, K>
+    >
+  : IsOptionalJoin<K, OptionalJoins> extends true
+    ? K extends keyof Base
+      ? // We need to include the base type if it exists for owning sides of relations.
+        | ResolveTypeFromExpansion<
+              Expansion[K],
+              unknown,
+              FilterOptionalJoins<OptionalJoins, K>
+            >
+          | Base[K]
+          | undefined
+      :
+          | ResolveTypeFromExpansion<
+              Expansion[K],
+              unknown,
+              FilterOptionalJoins<OptionalJoins, K>
+            >
+          | undefined
+    : K extends keyof Base
+      ? Base[K]
+      : never;
 
 /**
  * Provided a Definition and a QueryBuilder, resolves the return type.
@@ -240,16 +271,30 @@ type ResolveExpansionKey<
  * For usage of this type in  function parameter definitions, OptionalJoins can be supplied.
  */
 export type QueryBuilderResolver<
-	DefinitionType,
-	QueryBuilder,
-	OptionalJoins extends string = "",
+  DefinitionType,
+  QueryBuilder,
+  OptionalJoins extends string = "",
 > =
-		DefinitionType extends QueryBuilderDefinition<infer Base, infer Expansion>
-		? _ResolveType<ResolveBaseResult<Base, QueryBuilder, OptionalJoins> &
-			ConvertNeverAndUndefined<_ResolveType<{
-				[K in Exclude<Exclude<
-					keyof Expansion,
-					QueryBuilderSpecialKeys
-				>, number | symbol>]: _ResolveType<ResolveExpansionKey<K, Base, Expansion, QueryBuilder, OptionalJoins>>
-			}>>>
-		: never;
+  DefinitionType extends QueryBuilderDefinition<infer Base, infer Expansion>
+    ? _ResolveType<
+        ResolveBaseResult<Base, QueryBuilder, OptionalJoins> &
+					OmitNever<
+						ConvertUndefinedToPartial<
+							_ResolveType<{
+								[K in Exclude<
+									Exclude<keyof Expansion, QueryBuilderSpecialKeys>,
+									number | symbol
+								>]: _ResolveType<
+									ResolveExpansionKey<
+										K,
+										Base,
+										Expansion,
+										QueryBuilder,
+										OptionalJoins
+									>
+								>;
+							}>
+						>
+					>
+      >
+    : never;
